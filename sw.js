8e50dc0c35e88e9d7e1b8dc1354ba4a3fdc4428e
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2019-12-04 15:16:52 -0600

    PWA changes for profile

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE_NAME = 'my-profile';
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/image.png',
+  '/style.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
