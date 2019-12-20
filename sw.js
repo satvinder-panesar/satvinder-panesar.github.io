431074f8629e8fa4813f38d5f60b4f66c788356c
Panesar, Satvinder Singh (US - Dallas) <spanesar@deloitte.com>
2019-12-20 17:22:00 -0600

    network then cache

--- sw.js
+++ sw.js
@@ -17,15 +17,16 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            return fetch(event.request);
-        })
-        .catch(function(error) {
-            caches.match(event.request)
-            .then(function(response) {
-                return response;
-            })
-        })
-    );
+		fetch(event.request)
+		  .then(function(res) {
+			return caches.open(CACHE_NAME)
+			  .then(function(cache) {
+				cache.put(event.request.url, res.clone());
+				return res;
+			})
+		})
+		.catch(function(err) {
+			return caches.match(event.request);
+		})
+	);
 });
